6b5cc6de2ebbc168f88c6c2d8a537f0d8b296a60
GuoXiaodong <gxd@51888.cn>
2021-08-09 16:43:20 +0800

    /**remindView*/

---
+++ YueYuanAPP/ViewControllers/Login/V/LoginRemindView/YYLoginRemindView.h
@@ -0,0 +1,16 @@
+//
+//  YYLoginRemindView.h
+//  YueYuanAPP
+//
+//  Created by 约牛 on 2021/8/9.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YYLoginRemindView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YueYuanAPP/ViewControllers/Login/V/LoginRemindView/YYLoginRemindView.m
@@ -0,0 +1,20 @@
+//
+//  YYLoginRemindView.m
+//  YueYuanAPP
+//
+//  Created by 约牛 on 2021/8/9.
+//
+
+#import "YYLoginRemindView.h"
+
+@implementation YYLoginRemindView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
